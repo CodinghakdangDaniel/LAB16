b5ec97ea2d36cbe33ba29d1e30e9b86b2aed3182
CodinghakdangDaniel <junh2e97@gmail.com>
2023-09-15 18:06:23 +0900

    lab16

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ let details1 = document.getElementById('details1');
 let details2 = document.getElementById('details2');
 let details3 = document.getElementById('details3');
 
-function showAll () {
+function showAll() {
     if (details1.style.display  === 'none') {
         details1.style.display = 'block';
-        details1.style.transition = 'all 2s';  
+        // details1.style.transition = 'all 2s';  
     }
     else {
         details1.style.display = 'none';
     }
 };
 
-function showAll2 () {
+function showAll2() {
     if (details2.style.display === 'none') {
         details2.style.display = 'block';
     }
@@ -21,11 +21,30 @@ function showAll2 () {
     }
 };
 
-function showAll3 () {
-        if(details3.style.display === 'none') {
+function showAll3() {
+    if(details3.style.display === 'none') {
         details3.style.display = 'block';
-        }
-        else {
+    }
+    else {
             details3.style.display = 'none';
-        }
+    }
 };
+
+
+// let detail = document.getElementsByClassName('detail');
+// console.log(detail);
+
+// // let detailArray = [`${details1}`, `${details2}`, `${details3}`]
+// // console.log(detailArray[0]);
+// function showAll () {
+// for ( var i = 0; i < detail.length; i++) { 
+//     if (detail[0].style.display = 'none' ) {
+//         detail[0].style.display = 'block'
+//         // console.log(detail[i]);
+//     }
+//     else if (detail[0].style.display === 'block') {
+//         detail[0].style.display = 'none'
+//         console.log(detail[i]);
+//     }
+// }
+// }
